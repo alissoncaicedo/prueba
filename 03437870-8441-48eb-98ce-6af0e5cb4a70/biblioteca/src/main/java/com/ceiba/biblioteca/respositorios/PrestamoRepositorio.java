765772591684531116ce76c6f51f6b4f787e613f
alisson manuela caicedo cardona <manucaicedo1205@gmail.com>
2023-05-31 16:09:19 -0500

    agregue un id y importe el gradel

--- 03437870-8441-48eb-98ce-6af0e5cb4a70/biblioteca/src/main/java/com/ceiba/biblioteca/respositorios/PrestamoRepositorio.java
+++ 03437870-8441-48eb-98ce-6af0e5cb4a70/biblioteca/src/main/java/com/ceiba/biblioteca/respositorios/PrestamoRepositorio.java
@@ -1,17 +1,29 @@
 package com.ceiba.biblioteca.respositorios;
 
 public class PrestamoRepositorio {
+
+    private Integer id;
     private String ISBN;
     private String identificadordeusuario;
     private Integer tipodeusuario;
 
     public PrestamoRepositorio() {
+
     }
 
-    public PrestamoRepositorio(String ISBN, String identificadordeusuario, Integer tipodeusuario) {
+    public PrestamoRepositorio(Integer id, String ISBN, String identificadordeusuario, Integer tipodeusuario) {
+        this.id = id;
         this.ISBN = ISBN;
         this.identificadordeusuario = identificadordeusuario;
         this.tipodeusuario = tipodeusuario;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getISBN() {
